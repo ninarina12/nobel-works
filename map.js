c322000a45db61f684b78565045a7c6536a8777b
ninarina12 <31999567+ninarina12@users.noreply.github.com>
2017-10-09 10:08:29 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,22 +1,26 @@
-var sample_data = [
-    {"year": "1994", "country": "eusrb", "name": "Serbia", "tags": "birth country", "color": "#ABDDA6"},
-    {"year": "2009", "country": "eufra", "name": "France", "tags": "travel", "color": "#ABDDA4"},
-    {"year": "2010", "country": "namex", "name": "Mexico", "tags": "travel", "color": "#ABDDA4"},
-    {"year": "2014", "country": "euita", "name": "Italy", "tags": "travel", "color": "#ABDDA4"},
-    {"year": "2017", "country": "nausa", "name": "United States", "tags": "research", "color": "#ABDDA4"}
-];
+var basic_choropleth = new Datamap({
+  element: document.getElementById("svg_map"),
+  projection: 'mercator',
+  fills: {
+    defaultFill: "#CCCCCC",
+    authorHasTraveledTo: "#ABDDA4"
+  },
+  data: {
+    CHN: { fillKey: "authorHasTraveledTo", year: "1992 - 2014" },
+    DEU: { fillKey: "authorHasTraveledTo", year: "2014" },
+    USA: { fillKey: "authorHasTraveledTo", year: "2014 - 2017" },
+    BHS: { fillKey: "authorHasTraveledTo", year: "2016" },
+    CHL: { fillKey: "authorHasTraveledTo", year: "2017" }
+  },
+  geographyConfig: {
+    popupTemplate: function(geo, data) {
+        return ['<div class="hoverinfo"><strong>',
+                'Country: ' + geo.properties.name,
+                '<br> Year: ' + data.year,
+                '</strong></div>'].join('');
+    }
+  }
+});
 
-// Add a tooltip to show the year and the tags fields from your dataset
-var visualization = d3plus.viz()
-    .container("#svg_map")        // container DIV to hold the visualization
-    .data(sample_data)            // data to use with the visualization
-    .coords({
-        "mute": ["anata"],
-        "value": "http://d3plus.org/topojson/countries.json"
-    })                            // pass topojson coordinates
-    .type("geo_map")              // visualization type
-    .id("country")                // key for which our data is unique on
-    .text("name")                 // key to use for display text
-    .color("color")               // key for coloring countries
-    .tooltip(['year','tags'])
-    .draw();                      // finally, draw the visualization!
+// Add a legend to show the color of all categories included in the dataset
+var legend = d3.select("#svg_map").append("svg").attr("id", "legend");
