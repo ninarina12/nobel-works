8af2ccad41525af233b9de98e5ab9c861dab4c9a
Nina Andrejevic <nina12@mit.edu>
2020-03-19 03:54:40 -0400

    Updated index and js.

--- visualization.js
+++ visualization.js
@@ -52,7 +52,7 @@ d3.json("data/data.json", function(error, root) {
       .enter().append("g")
         .attr("class", "legend")
         .append("text")
-          //.attr("x", -diameter / 2.5)
+          .attr("x", 100)
           .attr("dy", 100)
           .attr("id", "title");
 
