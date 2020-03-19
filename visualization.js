a971cd461ec0513da0c37ace4428b41ae9c52b10
Nina Andrejevic <nina12@mit.edu>
2020-03-19 15:02:15 -0400

    Updated index and js.

--- visualization.js
+++ visualization.js
@@ -1,5 +1,5 @@
 var svg = d3.select("svg"),
-    margin = 20,
+    margin = 10,
     diameter = +svg.attr("width"),
     g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
