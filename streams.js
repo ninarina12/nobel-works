874bb09f85f0c56b3bd401017ba7e9444d77d776
Nina Andrejevic <nina12@mit.edu>
2020-03-20 18:12:07 -0400

    Updated index and js.

--- streams.js
+++ streams.js
@@ -1,79 +1,29 @@
-var svg = d3.select("body").select("#svg_streams"),
-    margin = 10,
-    diameter = +svg.attr("width"),
-    g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
-
-var color = d3.scaleOrdinal()
-    .domain([0, 1])
-    .range(["#1E2148", "#2A2D58", "#5A5D8D"]);
-
-var stars = d3.scaleOrdinal()
-    .domain([0, 1])
-    .range(["#D3B77A", "#9EC6F0", "#1D5A87", "#DE9951"])
-
-var pack = d3.pack()
-    .size([diameter - margin, diameter - margin])
-    .padding(function(d) {return 12 - d.depth});
-
-d3.json("data/data.json", function(error, root) {
-  if (error) throw error;
-
-  root = d3.hierarchy(root)
-      .sum(function(d) { return Math.sqrt(d.value); })
-      .sort(function(d) { return d.depth - Math.sqrt(d.value); });
-
-  var focus = root,
-      nodes = pack(root).descendants(),
-      view;
-
-  var circle = g.selectAll("stream")
-    .data(nodes.slice(4))
-    .enter().append("circle")
-      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
-      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
-      .style("stroke", function(d) { return d.children ? null : stars(d.r); })
-      .style("stroke-width", 2)
-      .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
-
-  var text = g.selectAll("stream_text")
-    .data(nodes)
-    .enter().append("text")
-      .attr("class", "label")
-      .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
-      .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
-      .text(function(d) { return d.children ? d.data.name.toUpperCase() : d.data.name.toUpperCase() + ": " + d.data.value; })
-      .style("fill", function(d) { return d.children ? "white" : color(d.depth); })
-      .style("text-shadow", function(d) { return d.children ? null : "0 1px 0 #fff, 1px 0 0 #fff, -1px 0 0 #fff, 0 -1px 0 #fff, 1px 1px 0 #fff, -1px -1px 0 #fff, -1px 1px 0 #fff, 1px -1px 0 #fff"; })
-      .attr("dy", function(d) { return -d.r; });
-
-  var node = g.selectAll("stream,stream_text");
-
-  svg
-      .style("background", color(0))
-      .on("click", function() { zoom(root); });
-
-  zoomTo([root.x, root.y, root.r * 2 + margin]);
-
-  function zoom(d) {
-    var focus0 = focus; focus = d;
-
-    var transition = d3.transition()
-        .duration(d3.event.altKey ? 7500 : 750)
-        .tween("zoom", function(d) {
-          var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
-          return function(t) { zoomTo(i(t)); };
-        });
-
-    transition.selectAll("text")
-      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-        .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
-        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
-  }
-
-  function zoomTo(v) {
-    var k = diameter / v[2]; view = v;
-    node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
-    circle.attr("r", function(d) { return d.r * k; });
+var basic_choropleth = new Datamap({
+  element: document.getElementById("svg_streams"),
+  projection: 'mercator',
+  fills: {
+    defaultFill: "#CCCCCC",
+    authorHasTraveledTo: "#ABDDA4",
+    authorHasStudiedIn: "#AB55A4"
+  },
+  data: {
+    CHN: { fillKey: "authorHasStudiedIn", year: "1992 - 2014" },
+    DEU: { fillKey: "authorHasTraveledTo", year: "2014" },
+    USA: { fillKey: "authorHasStudiedIn", year: "2014 - 2017" },
+    BHS: { fillKey: "authorHasTraveledTo", year: "2016" },
+    CHL: { fillKey: "authorHasTraveledTo", year: "2017" }
+  },
+  geographyConfig: {
+    popupTemplate: function(geo, data) {
+        return ['<div class="hoverinfo"><strong>',
+                'Country: ' + geo.properties.name,
+                '<br> Year: ' + data.year,
+                '</strong></div>'].join('');
+    }
   }
 });
+var legend = d3.select("#svg_streams").append("svg").attr("id", "legend");
+legend.append("rect").attr("x", "200").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", "#ABDDA4");
+legend.append("text").attr("x", "225").attr("y", "24").attr("font-size", "12px").text("Traveled to");
+legend.append("rect").attr("x", "350").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", "#AB55A4");
+legend.append("text").attr("x", "375").attr("y", "24").attr("font-size", "12px").text("Studied in");
